739e4e4de6f5ce83d112ccf7fa086f3d50eb65e9
minaaaasftw <omranimina135@gmail.com>
2015-09-23 15:28:54 +0330

    sec definition

--- www/js/services.js
+++ www/js/services.js
@@ -1,65 +1,67 @@
-///**
-// * Created by Mina on 9/23/2015.
-// */
-//var services= angular.module('services',[]);
-//services.factory('modes',function()
-//{
-//  var factory=fuction()
-//  {
-//    var output={};
-//
-//
-//
-//    output.Mode_Date={
-//
-//      year:0,
-//      mounth:0,
-//      day:0
-//
-//    };
-//
-//    output.Mode_Date_Time= {
-//
-//      year:0,
-//      mounth:0,
-//      day:0,
-//      hour:0,
-//      minute:0,
-//      second:0
-//    };
-//
-//    output.Mode_SimpleNum= {
-//
-//     num:0
-//
-//    };
-//
-//    output.Mode_Piece={
-//
-//      year:0,
-//      mounth:0,
-//      day:0,
-//      hour:0,
-//      minute:0,
-//      second:0
-//
-//    };
-//    output.Mode_Time={
-//
-//      day:0,
-//      hour:0,
-//      minute:0,
-//      second:0
-//
-//    };
-//
-//
-//    output.printResult = function()
-//    {
-//      console.log();
-//    };
-//    return output;
-//
-//  }
-//});
-//
+/**
+ * Created by Mina on 9/23/2015.
+ */
+var services= angular.module('services',[]);
+services.factory('modes',function()
+{
+  var factory=function()
+  {
+    var output={};
+
+
+
+    output.Mode_Date={
+
+      year:0,
+      month:0,
+      day:0
+
+    };
+
+    output.Mode_Date_Time= {
+
+      year:0,
+      month:0,
+      day:0,
+      hour:0,
+      minute:0,
+      second:0
+    };
+
+    output.Mode_SimpleNum= {
+
+     num:0
+
+    };
+
+    output.Mode_Piece={
+
+      year:0,
+      month:0,
+      day:0,
+      hour:0,
+      minute:0,
+      second:0
+
+    };
+    output.Mode_Time={
+
+      day:0,
+      hour:0,
+      minute:0,
+      second:0
+
+    };
+
+
+    output.printResult = function()
+    {
+      console.log();
+    };
+    return output;
+
+  };
+
+  return factory;
+});
+
