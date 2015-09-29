2826297fd996c6a3fb2b18299187ecb116a70587
hokafi <h.kafi.123@gmail.com>
2015-09-29 12:27:00 +0330

    time-enabled created!

--- www/js/app.js
+++ www/js/app.js
@@ -5,330 +5,261 @@
 // the 2nd parameter is an array of 'requires'
 
 
-angular.module('starter', ['ionic'])
+var app=angular.module('starter', ['ionic','services']);
 
-  .run(function($ionicPlatform) {
-    $ionicPlatform.ready(function() {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      if(window.cordova && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      }
-      if(window.StatusBar) {
-        StatusBar.styleDefault();
-      }
-    });
-  })
-  .controller('basic', function ($scope) {
- //   $scope.matrix();
-    console.log($scope.number);
-    //time_reg($scope.number);
-    //date_reg($scope.number);
-    $scope.temp='';
-    $scope.number='';
-    $scope.num=function($n)
-    {
-      $scope.number += $n;
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    $scope.time_reg = function($num,$s)
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+app.controller('basic', function ($scope,regex,enabled){
+  $scope.disPlus=true;
+  //   $scope.matrix();
+  console.log($scope.number);
+  //time_reg($scope.number);
+  //date_reg($scope.number);
+  $scope.temp='';
+  $scope.number='';
+  $scope.num=function($n)
+  {
+    $scope.number += $n;
+  }
+//-------------------------------------------------------------------------------------------
+  $scope.date_time_reg = function($num,$s)
+  {
+    var c=$num + $s;
+    if(/\d{4}\/(?:[12]\d|(3[01])|(01|02|03|04|05|06|07|08|09)|(1|2|3|4|5|6|7|8|9))\/(?:(01|02|03|04|05|06|07|08|09)|(1|2|3|4|5|6|7|8|9)|1[012])\/(?:[01]?\d|2[0-3]):(0|1|2|3|4|5)?\d:(0|1|2|3|4|5)?\d$/.test(c))
     {
-      var c=$num+$s;
-      console.log($num);
-      //var path=/([0-1][0-9])|2[0-3]/;
-      //var path=/(00|01|02|03|04|05|06|07|08|09|10|11|12|13|14|15|16|17|18|19|20|21|22|23)/;
-      if(/^(?:[01]?\d|2[0-3]):(0|1|2|3|4|5)?\d:(0|1|2|3|4|5)?\d$/.test(c))
-      {
-       // enabled.output.timeEnabled($num,$s);
-        console.log("true1");
-        return 2;
-      }
-      else if(/^(?:[01]?\d|2[0-3]):(0|1|2|3|4|5)?\d:$/.test(c))
-      {
-        console.log("true2")
-        return 1;
-      }
-      else if(/^(?:[01]?\d|2[0-3]):(0|1|2|3|4|5)?\d$/.test(c))
-      {
-        console.log("true3")
-        return 1;
-      }
-      else if(/^(?:[01]?\d|2[0-3]):$/.test(c))
-      {
-        console.log("true4")
-        return 1;
-      }
-      else if(/^(?:[01]?\d|2[0-3])$/.test(c))
-      {
-        console.log("true5");
-        return 1;
-      }
-      else if(/\d{1}/.test(c))
-      {
-        console.log("true6")
-        return 1;
-      }
-      else {
-        return 0;
-        console.log(false);
-      }
+      console.log("true1");
+      return 2;
+    }
+    else if(/\d{4}\/(?:[12]\d|(3[01])|(01|02|03|04|05|06|07|08|09)|(1|2|3|4|5|6|7|8|9))\/$/.test(c))
+    {
+      console.log("true2");
+      return 1;
+    }
+    else if(/\d{4}\/(?:[12]\d|(3[01])|(01|02|03|04|05|06|07|08|09)|(1|2|3|4|5|6|7|8|9))$/.test(c))
+    {
+      console.log("true3");
+      return 1;
+    }
+    else if(/\d{4,5}\/$/.test(c))
+    {
+      console.log("true4");
+      return 1;
+    }
+    else if(/\d{4}$/.test(c))
+    {
+      console.log("true5");
+      return 1;
     }
 
-//-------------------------------------------------------------------------------------------
-    $scope.date_reg = function($num,$s)
+    if(/^(?:[01]?\d|2[0-3]):(0|1|2|3|4|5)?\d:(0|1|2|3|4|5)?\d$/.test(c))
     {
-      var c=$num+$s;
-      if(/\d{4}\/(?:[12]\d|(3[01])|(01|02|03|04|05|06|07|08|09)|(1|2|3|4|5|6|7|8|9))\/(?:(01|02|03|04|05|06|07|08|09)|(1|2|3|4|5|6|7|8|9)|1[012])$/.test(c))
-      {
-        console.log("true1");
-        return 2;
-      }
-      else if(/\d{4}\/(?:[12]\d|(3[01])|(01|02|03|04|05|06|07|08|09)|(1|2|3|4|5|6|7|8|9))\/$/.test(c))
-      {
-        console.log("true2");
-        return 1;
-      }
-      else if(/\d{4}\/(?:[12]\d|(3[01])|(01|02|03|04|05|06|07|08|09)|(1|2|3|4|5|6|7|8|9))$/.test(c))
-      {
-        console.log("true3");
-        return 1;
-      }
-      else if(/\d{4,5}\/$/.test(c))
-      {
-        console.log("true4");
-      }
-      else if(/\d{4}$/.test(c))
-      {
-        console.log("true5");
-      }
-    };
-
-//-------------------------------------------------------------------------------------------
-    $scope.date_time_reg = function($num,$s)
+      console.log("true1");
+      return 1;
+    }
+    else if(/^(?:[01]?\d|2[0-3]):(0|1|2|3|4|5)?\d:$/.test(c))
     {
-      var c=$num + $s;
-      if(/\d{4}\/(?:[12]\d|(3[01])|(01|02|03|04|05|06|07|08|09)|(1|2|3|4|5|6|7|8|9))\/(?:(01|02|03|04|05|06|07|08|09)|(1|2|3|4|5|6|7|8|9)|1[012])\/(?:[01]?\d|2[0-3]):(0|1|2|3|4|5)?\d:(0|1|2|3|4|5)?\d$/.test(c))
-      {
-        console.log("true1");
-        return 2;
-      }
-      else if(/\d{4}\/(?:[12]\d|(3[01])|(01|02|03|04|05|06|07|08|09)|(1|2|3|4|5|6|7|8|9))\/$/.test(c))
-      {
-        console.log("true2");
-        return 1;
-      }
-      else if(/\d{4}\/(?:[12]\d|(3[01])|(01|02|03|04|05|06|07|08|09)|(1|2|3|4|5|6|7|8|9))$/.test(c))
-      {
-        console.log("true3");
-        return 1;
-      }
-      else if(/\d{4,5}\/$/.test(c))
-      {
-        console.log("true4");
-        return 1;
-      }
-      else if(/\d{4}$/.test(c))
-      {
-        console.log("true5");
-        return 1;
-      }
-
-      if(/^(?:[01]?\d|2[0-3]):(0|1|2|3|4|5)?\d:(0|1|2|3|4|5)?\d$/.test(c))
-      {
-        console.log("true1");
-        return 1;
-      }
-      else if(/^(?:[01]?\d|2[0-3]):(0|1|2|3|4|5)?\d:$/.test(c))
-      {
-        console.log("true2")
-        return 1;
-      }
-      else if(/^(?:[01]?\d|2[0-3]):(0|1|2|3|4|5)?\d$/.test(c))
-      {
-        console.log("true3")
-        return 1;
-      }
-      else if(/^(?:[01]?\d|2[0-3]):$/.test(c))
-      {
-        console.log("true4")
-        return 1;
-      }
-      else if(/^(?:[01]?\d|2[0-3])$/.test(c))
-      {
-        console.log("true5")
-        return 1;
-      }
-      else if(/\d{1}/.test(c))
-      {
-        console.log("true6")
-        return 1;
-      }
-      else{
-        console.log(false);
-      }
-    };
+      console.log("true2")
+      return 1;
+    }
+    else if(/^(?:[01]?\d|2[0-3]):(0|1|2|3|4|5)?\d$/.test(c))
+    {
+      console.log("true3")
+      return 1;
+    }
+    else if(/^(?:[01]?\d|2[0-3]):$/.test(c))
+    {
+      console.log("true4")
+      return 1;
+    }
+    else if(/^(?:[01]?\d|2[0-3])$/.test(c))
+    {
+      console.log("true5")
+      return 1;
+    }
+    else if(/\d{1}/.test(c))
+    {
+      console.log("true6")
+      return 1;
+    }
+    else{
+      console.log(false);
+    }
+  };
 
 //--------------------------------------------------------------------------------------------
-    $scope.matrix=function()
-    {
-      ////////////////////////created by Sarah
-      var i;
-      var plusArray = new Array(5);
-      for (i = 0; i < 5; i++)
-        plusArray[i] = new Array(5);
-      plusArray[0][0] = 1;
-      plusArray[0][1] = 2;
-      plusArray[0][2] = 4;
-      plusArray[0][3] = 4;
-      plusArray[0][4] = 0;
-      plusArray[1][0] = 2;
-      plusArray[1][1] = 0;
-      plusArray[1][2] = 4;
-      plusArray[1][3] = 0;
-      plusArray[1][4] = 0;
-      plusArray[2][0] = 4;
-      plusArray[2][1] = 0;
-      plusArray[2][2] = 0;
-      plusArray[2][3] = 0;
-      plusArray[2][4] = 0;
-      plusArray[3][0] = 4;
-      plusArray[3][1] = 0;
-      plusArray[3][2] = 0;
-      plusArray[3][3] = 0;
-      plusArray[3][4] = 0;
-      plusArray[4][0] = 0;
-      plusArray[4][1] = 0;
-      plusArray[4][2] = 0;
-      plusArray[4][3] = 0;
-      plusArray[4][4] = 5;
+  $scope.matrix=function()
+  {
+    ////////////////////////created by Sarah
+    var i;
+    var plusArray = new Array(5);
+    for (i = 0; i < 5; i++)
+      plusArray[i] = new Array(5);
+    plusArray[0][0] = 1;
+    plusArray[0][1] = 2;
+    plusArray[0][2] = 4;
+    plusArray[0][3] = 4;
+    plusArray[0][4] = 0;
+    plusArray[1][0] = 2;
+    plusArray[1][1] = 0;
+    plusArray[1][2] = 4;
+    plusArray[1][3] = 0;
+    plusArray[1][4] = 0;
+    plusArray[2][0] = 4;
+    plusArray[2][1] = 0;
+    plusArray[2][2] = 0;
+    plusArray[2][3] = 0;
+    plusArray[2][4] = 0;
+    plusArray[3][0] = 4;
+    plusArray[3][1] = 0;
+    plusArray[3][2] = 0;
+    plusArray[3][3] = 0;
+    plusArray[3][4] = 0;
+    plusArray[4][0] = 0;
+    plusArray[4][1] = 0;
+    plusArray[4][2] = 0;
+    plusArray[4][3] = 0;
+    plusArray[4][4] = 5;
 //****************************************
-      var minusArray = new Array(5);
-      for (i = 0; i < 5; i++)
-        minusArray[i] = new Array(5);
-      minusArray[0][0] = 1;
-      minusArray[0][1] = 0;
-      minusArray[0][2] = 0;
-      minusArray[0][3] = 0;
-      minusArray[0][4] = 0;
-      minusArray[1][0] = 2;
-      minusArray[1][1] = 0;
-      minusArray[1][2] = 0;
-      minusArray[1][3] = 0;
-      minusArray[1][4] = 0;
-      minusArray[2][0] = 4;
-      minusArray[2][1] = 0;
-      minusArray[2][2] = 0;
-      minusArray[2][3] = 0;
-      minusArray[2][4] = 0;
-      minusArray[3][0] = 4;
-      minusArray[3][1] = 0;
-      minusArray[3][2] = 0;
-      minusArray[3][3] = 0;
-      minusArray[3][4] = 0;
-      minusArray[4][0] = 0;
-      minusArray[4][1] = 0;
-      minusArray[4][2] = 0;
-      minusArray[4][3] = 0;
-      minusArray[4][4] = 5;
+    var minusArray = new Array(5);
+    for (i = 0; i < 5; i++)
+      minusArray[i] = new Array(5);
+    minusArray[0][0] = 1;
+    minusArray[0][1] = 0;
+    minusArray[0][2] = 0;
+    minusArray[0][3] = 0;
+    minusArray[0][4] = 0;
+    minusArray[1][0] = 2;
+    minusArray[1][1] = 0;
+    minusArray[1][2] = 0;
+    minusArray[1][3] = 0;
+    minusArray[1][4] = 0;
+    minusArray[2][0] = 4;
+    minusArray[2][1] = 0;
+    minusArray[2][2] = 0;
+    minusArray[2][3] = 0;
+    minusArray[2][4] = 0;
+    minusArray[3][0] = 4;
+    minusArray[3][1] = 0;
+    minusArray[3][2] = 0;
+    minusArray[3][3] = 0;
+    minusArray[3][4] = 0;
+    minusArray[4][0] = 0;
+    minusArray[4][1] = 0;
+    minusArray[4][2] = 0;
+    minusArray[4][3] = 0;
+    minusArray[4][4] = 5;
 //****************************************
-      var toArray = new Array(5);
-      for (i = 0; i < 5; i++)
-        toArray[i] = new Array(5);
-      toArray[0][0] = 0;
-      toArray[0][1] = 0;
-      toArray[0][2] = 0;
-      toArray[0][3] = 0;
-      toArray[0][4] = 0;
-      toArray[1][0] = 0;
-      toArray[1][1] = 1;
-      toArray[1][2] = 0;
-      toArray[1][3] = 0;
-      toArray[1][4] = 0;
-      toArray[2][0] = 0;
-      toArray[2][1] = 0;
-      toArray[2][2] = 1;
-      toArray[2][3] = 0;
-      toArray[2][4] = 0;
-      toArray[3][0] = 0;
-      toArray[3][1] = 0;
-      toArray[3][2] = 0;
-      toArray[3][3] = 1;
-      toArray[3][4] = 0;
-      toArray[4][0] = 0;
-      toArray[4][1] = 0;
-      toArray[4][2] = 0;
-      toArray[4][3] = 0;
-      toArray[4][4] = 0;
+    var toArray = new Array(5);
+    for (i = 0; i < 5; i++)
+      toArray[i] = new Array(5);
+    toArray[0][0] = 0;
+    toArray[0][1] = 0;
+    toArray[0][2] = 0;
+    toArray[0][3] = 0;
+    toArray[0][4] = 0;
+    toArray[1][0] = 0;
+    toArray[1][1] = 1;
+    toArray[1][2] = 0;
+    toArray[1][3] = 0;
+    toArray[1][4] = 0;
+    toArray[2][0] = 0;
+    toArray[2][1] = 0;
+    toArray[2][2] = 1;
+    toArray[2][3] = 0;
+    toArray[2][4] = 0;
+    toArray[3][0] = 0;
+    toArray[3][1] = 0;
+    toArray[3][2] = 0;
+    toArray[3][3] = 1;
+    toArray[3][4] = 0;
+    toArray[4][0] = 0;
+    toArray[4][1] = 0;
+    toArray[4][2] = 0;
+    toArray[4][3] = 0;
+    toArray[4][4] = 0;
 //***********************************
-      var multiplyArray = new Array(5);
-      for (i = 0; i < 5; i++)
-        multiplyArray[i] = new Array(5);
-      multiplyArray[0][0] = 0;
-      multiplyArray[0][1] = 0;
-      multiplyArray[0][2] = 0;
-      multiplyArray[0][3] = 0;
-      multiplyArray[0][4] = 1;
-      multiplyArray[1][0] = 0;
-      multiplyArray[1][1] = 0;
-      multiplyArray[1][2] = 0;
-      multiplyArray[1][3] = 0;
-      multiplyArray[1][4] = 0;
-      multiplyArray[2][0] = 0;
-      multiplyArray[2][1] = 0;
-      multiplyArray[2][2] = 0;
-      multiplyArray[2][3] = 0;
-      multiplyArray[2][4] = 0;
-      multiplyArray[3][0] = 0;
-      multiplyArray[3][1] = 0;
-      multiplyArray[3][2] = 0;
-      multiplyArray[3][3] = 0;
-      multiplyArray[3][4] = 0;
-      multiplyArray[4][0] = 1;
-      multiplyArray[4][1] = 0;
-      multiplyArray[4][2] = 0;
-      multiplyArray[4][3] = 0;
-      multiplyArray[4][4] = 5;
+    var multiplyArray = new Array(5);
+    for (i = 0; i < 5; i++)
+      multiplyArray[i] = new Array(5);
+    multiplyArray[0][0] = 0;
+    multiplyArray[0][1] = 0;
+    multiplyArray[0][2] = 0;
+    multiplyArray[0][3] = 0;
+    multiplyArray[0][4] = 1;
+    multiplyArray[1][0] = 0;
+    multiplyArray[1][1] = 0;
+    multiplyArray[1][2] = 0;
+    multiplyArray[1][3] = 0;
+    multiplyArray[1][4] = 0;
+    multiplyArray[2][0] = 0;
+    multiplyArray[2][1] = 0;
+    multiplyArray[2][2] = 0;
+    multiplyArray[2][3] = 0;
+    multiplyArray[2][4] = 0;
+    multiplyArray[3][0] = 0;
+    multiplyArray[3][1] = 0;
+    multiplyArray[3][2] = 0;
+    multiplyArray[3][3] = 0;
+    multiplyArray[3][4] = 0;
+    multiplyArray[4][0] = 1;
+    multiplyArray[4][1] = 0;
+    multiplyArray[4][2] = 0;
+    multiplyArray[4][3] = 0;
+    multiplyArray[4][4] = 5;
 //***************************************
-      var divisionArray = new Array(5);
-      for (i = 0; i < 5; i++)
-        divisionArray[i] = new Array(5);
-      divisionArray[0][0] = 0;
-      divisionArray[0][1] = 0;
-      divisionArray[0][2] = 0;
-      divisionArray[0][3] = 0;
-      divisionArray[0][4] = 1;
-      divisionArray[1][0] = 2;
-      divisionArray[1][1] = 0;
-      divisionArray[1][2] = 0;
-      divisionArray[1][3] = 0;
-      divisionArray[1][4] = 2;
-      divisionArray[2][0] = 0;
-      divisionArray[2][1] = 0;
-      divisionArray[2][2] = 0;
-      divisionArray[2][3] = 0;
-      divisionArray[2][4] = 3;
-      divisionArray[3][0] = 0;
-      divisionArray[3][1] = 0;
-      divisionArray[3][2] = 0;
-      divisionArray[3][3] = 0;
-      divisionArray[3][4] = 4;
-      divisionArray[4][0] = 0;
-      divisionArray[4][1] = 0;
-      divisionArray[4][2] = 0;
-      divisionArray[4][3] = 0;
-      divisionArray[4][4] = 5;
+    var divisionArray = new Array(5);
+    for (i = 0; i < 5; i++)
+      divisionArray[i] = new Array(5);
+    divisionArray[0][0] = 0;
+    divisionArray[0][1] = 0;
+    divisionArray[0][2] = 0;
+    divisionArray[0][3] = 0;
+    divisionArray[0][4] = 1;
+    divisionArray[1][0] = 2;
+    divisionArray[1][1] = 0;
+    divisionArray[1][2] = 0;
+    divisionArray[1][3] = 0;
+    divisionArray[1][4] = 2;
+    divisionArray[2][0] = 0;
+    divisionArray[2][1] = 0;
+    divisionArray[2][2] = 0;
+    divisionArray[2][3] = 0;
+    divisionArray[2][4] = 3;
+    divisionArray[3][0] = 0;
+    divisionArray[3][1] = 0;
+    divisionArray[3][2] = 0;
+    divisionArray[3][3] = 0;
+    divisionArray[3][4] = 4;
+    divisionArray[4][0] = 0;
+    divisionArray[4][1] = 0;
+    divisionArray[4][2] = 0;
+    divisionArray[4][3] = 0;
+    divisionArray[4][4] = 5;
 //**********************************
-    }
+  }
 //--------------------------------------------------------------------------------------------
 
-    $scope.check=function($s)
+  $scope.check=function($s)
+  {
+    $scope.disPlus=true;
+    var t = regex.time_reg($scope.temp,$s);
+    var d = regex.date_reg($scope.temp,$s);
+    if(t==2)
     {
-      var t = $scope.time_reg($scope.temp,$s);
-      var d = $scope.date_reg($scope.temp,$s);
-      if(t)
-      {
+      $scope.disPlus=false;
+    }
+    if(t)
 
-;
-      }
       if(t|d)
       {
         $scope.num($s);
@@ -338,5 +269,5 @@ angular.module('starter', ['ionic'])
       {
         console.log("false");
       }
-    }
-  });
+  }
+});
--- www/js/enabled.js
+++ www/js/enabled.js
@@ -1,19 +1,22 @@
 /**
  * Created by Hossein on 9/29/2015.
  */
-var enabled= angular.module('enabled',['services']);
-services.factory('enabled',function() {
-  var factory = function () {
+app.factory('enabled',function(){
     var output = {};
     output.flags={
       time:0,
       date:0
-    }
+    };
+
     output.timeEnabled=function($string,$s2)
     {
-;
+      console.log("ff");
+      if($s2==':')
+      {
+        //$scope.disTime=true;
+      }
     };
+
     return output;
-  }
-  return factory;
+
 });
